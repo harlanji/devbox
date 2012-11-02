64947fa436a8f332c3ecad8368d8f097dd105abe
Harlan Iverson <h.ivrson@gmail.com>
2012-11-01 21:37:10 -0700

    added missing .vimrc which is needed to infect with pathogen, and fixed incorrect pathogen.vim link

--- postinstall.sh
+++ postinstall.sh
@@ -5,6 +5,6 @@ ROOT="$PWD"
 ln -sf "/usr/lib/go/misc/vim" "$ROOT/.vim/bundle/vim-golang"
 ln -sf "../../.vim/gocode/vim" "$ROOT/.vim/bundle/vim-gocode"
 
-ln -sf "../../.vim/vim-pathogen/pathogen.vim" "$ROOT/.vim/autoload/pathogen.vim"
+ln -sf "../../.vim/vim-pathogen/autoload/pathogen.vim" "$ROOT/.vim/autoload/pathogen.vim"
 
 
